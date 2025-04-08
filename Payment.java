2e5c62880f804f2bbddc17e4eae6ec67e32f4e5a
SecretOne12 <155071082+SecretOne12@users.noreply.github.com>
2025-04-08 21:41:35 +0700

    Add files via upload

---
+++ Payment.java
@@ -0,0 +1,39 @@
+public class Payment {
+    // Attribute
+    protected double totalPrice;
+    protected String paymentMethod;
+    protected String paymentStatus;
+
+    // Constructor
+    public Payment(double totalPrice, String paymentMethod) {
+        this.totalPrice = totalPrice;
+        this.paymentMethod = paymentMethod;
+        this.paymentStatus = "Pending";
+    }
+
+    //overloading bank transfer dan credit card
+    public void pay(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+        this.paymentStatus = "Pembayaran Berhasil dengan " + paymentMethod;
+    }
+
+    //overloading diskon
+    public void pay(String paymentMethod, double discountPercent) {
+        this.paymentMethod = paymentMethod;
+        double discount = totalPrice * (discountPercent / 100);
+        totalPrice -= discount;
+        this.paymentStatus = "Pembayaran Berhasil dengan " + paymentMethod + " (Diskon " + discountPercent + "%)";
+    }
+
+
+    // Void Method Menampilkan detail pembayaran
+    public void displayPaymentDetails() {
+        System.out.println("Metode Pembayaran: " + paymentMethod);
+        System.out.println("Status Pembayaran: " + paymentStatus);
+    }
+
+    // return type totalprice setelah diskon (jika pakai voucher)
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+}
